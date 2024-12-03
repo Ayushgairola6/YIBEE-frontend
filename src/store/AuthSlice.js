d87e53a7c5872743a784f57ddcca394fd8484057
ayush <ayushgairola2002@gmail.com>
2024-12-03 21:14:57 +0530

    UPDATED FRONTEND date:3/12/2024

--- src/store/AuthSlice.js
+++ src/store/AuthSlice.js
@@ -6,7 +6,7 @@ export const Signup = createAsyncThunk(
     'post/signupRqst',
     async (SignupData, thunkAPI) => {
         try {
-            const response = await axios.post('https://yibee.vercel.app/api/signup', SignupData, {
+            const response = await axios.post('https://yibee.vercel.app/api/auth/signup', SignupData, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
@@ -23,7 +23,7 @@ export const LoginRqst = createAsyncThunk(
     'post/loginRqst',
     async (LoginData, thunkAPI) => {
         try {
-            const response = await axios.post('https://yibee.vercel.app/api/login', LoginData, {
+            const response = await axios.post('https://yibee.vercel.app/api/auth/login', LoginData, {
                 headers: {
                     'Content-Type': 'application/json',
 
